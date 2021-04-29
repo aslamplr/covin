5957ff77ca4664c30013f3495142f1de8ccd6a04
Aslam Ahammed <aslamplr@gmail.com>
2021-04-29 20:35:32 +0530

    Make vaccine optional for centers endpoint

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,18 +1,17 @@
 mod problem;
 
-use std::env;
 use anyhow::Result;
-use serde::Deserialize;
-use warp::Filter;
 use once_cell::sync::Lazy;
+use serde::Deserialize;
+use std::env;
 use tracing_subscriber::fmt::format::FmtSpan;
+use warp::Filter;
 
 static BASE_URL: Lazy<String> = Lazy::new(|| env::var("BASE_URL").unwrap());
 static DISTRICTS_URL: Lazy<String> = Lazy::new(|| env::var("DISTRICTS_URL").unwrap());
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     // Filter traces based on the RUST_LOG env var, or, if it's not set,
     // default to show the output of the example.
     let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
@@ -22,7 +21,6 @@ async fn main() -> Result<()> {
         .with_env_filter(filter)
         .with_span_events(FmtSpan::CLOSE)
         .init();
-
 
     let districts = warp::path("districts")
         .and(warp::get())
@@ -47,10 +45,16 @@ async fn main() -> Result<()> {
                  date,
                  vaccine,
              }| async move {
-                let centers = get_all_centers_by_district(&district_id, &date, &vaccine)
+                let centers = get_all_centers_by_district(&district_id, &date, vaccine.as_deref())
                     .await
                     .map_err(problem::build)?;
-                tracing::info!("centers: \n{}", centers);
+                tracing::info!(
+                    "centers: date={}; district_id={}; vaccine={}; \n{}",
+                    date,
+                    district_id,
+                    vaccine.as_deref().unwrap_or_else(|| "*"),
+                    centers
+                );
                 Ok::<_, warp::reject::Rejection>(warp::reply::with_header(
                     centers,
                     "Content-Type",
@@ -72,7 +76,7 @@ async fn main() -> Result<()> {
         .with(cors)
         .with(warp::trace::request());
 
-    // To serve locally uncomment the following 
+    // To serve locally uncomment the following
     // warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
 
     warp_lambda::run(warp::service(routes)).await?;
@@ -81,29 +85,29 @@ async fn main() -> Result<()> {
 }
 
 async fn get_all_districts() -> Result<String> {
-    let districts = reqwest::get(&*DISTRICTS_URL)
-        .await?
-        .text()
-        .await?;
+    let districts = reqwest::get(&*DISTRICTS_URL).await?.text().await?;
     Ok(districts)
 }
 
 async fn get_all_centers_by_district(
     district_id: &str,
     date: &str,
-    vaccine: &str,
+    vaccine: Option<&str>,
 ) -> Result<String> {
+    let query = {
+        let mut query = vec![("district_id", district_id), ("date", date)];
+        if vaccine.is_some() {
+            query.push(("vaccine", vaccine.unwrap()))
+        }
+        query
+    };
     let client = reqwest::Client::new();
     let centers = client
         .get(format!(
             "{}/{}",
             *BASE_URL, "v2/appointment/sessions/calendarByDistrict"
         ))
-        .query(&[
-            ("district_id", district_id),
-            ("date", date),
-            ("vaccine", vaccine),
-        ])
+        .query(&query)
         .send()
         .await?
         .text()
@@ -115,5 +119,5 @@ async fn get_all_centers_by_district(
 struct CenterQueryParams {
     district_id: String,
     date: String,
-    vaccine: String,
+    vaccine: Option<String>,
 }
