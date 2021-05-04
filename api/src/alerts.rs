6e69818a4d0a551549b2673255dcb4136fc1ca12
Aslam Ahammed <aslamplr@gmail.com>
2021-05-04 18:59:16 +0530

    Implement better error handling

--- api/src/alerts.rs
+++ api/src/alerts.rs
@@ -1,12 +1,19 @@
 use crate::problem;
 use dynomite::{
     attr_map,
-    dynamodb::{DynamoDb, DynamoDbClient, GetItemInput, PutItemInput},
+    dynamodb::{
+        DeleteItemError, DeleteItemInput, DynamoDb, DynamoDbClient, GetItemError, GetItemInput,
+        PutItemError, PutItemInput,
+    },
     retry::{Policy, RetryingDynamoDb},
-    Attributes, FromAttributes as _, Item, Retries,
+    AttributeError, Attributes, FromAttributes as _, Item, Retries,
 };
+use rusoto_core::RusotoError;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 use warp::Filter;
+
+const TABLE_NAME: &str = "CovinAlerts";
 
 pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let retry_policy = Policy::Pause(3, std::time::Duration::from_millis(10));
@@ -24,18 +31,18 @@ pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejecti
 
                 let res = dynamo_db
                     .get_item(GetItemInput {
-                        table_name: "CovinAlerts".to_string(),
+                        table_name: TABLE_NAME.to_string(),
                         key,
                         ..GetItemInput::default()
                     })
                     .await
-                    .ok()
-                    .map(|res| res.item)
-                    .flatten()
-                    .map(|mut item| Alert::from_attrs(&mut item).ok())
-                    .flatten()
-                    .ok_or(anyhow::anyhow!("Unable to fetch alert for user!"))
-                    .map_err(problem::build)?;
+                    .map_err(build_err)
+                    .map(|res| {
+                        res.item
+                            .map(|mut item| Alert::from_attrs(&mut item).map_err(build_err))
+                            .ok_or(AlertError::NothingFound)
+                            .map_err(build_err)
+                    })???;
                 Ok::<_, warp::Rejection>(warp::reply::json(&res))
             },
         );
@@ -49,12 +56,12 @@ pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejecti
                 let alert: Alert = alert_payload.into();
                 dynamo_db
                     .put_item(PutItemInput {
-                        table_name: "CovinAlerts".to_string(),
+                        table_name: TABLE_NAME.to_string(),
                         item: alert.into(),
                         ..PutItemInput::default()
                     })
                     .await
-                    .map_err(problem::build)?;
+                    .map_err(build_err)?;
                 Ok::<_, warp::Rejection>(warp::reply::with_status(
                     warp::reply::reply(),
                     warp::http::StatusCode::CREATED,
@@ -62,22 +69,51 @@ pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejecti
             },
         );
 
-    let update_alert = warp::path!(String)
-        .and(warp::put())
-        .and(warp::body::json::<AlertPayload>())
-        .and(dynamo_db.clone())
-        .map(|user_id, alert_req, _dynamo_db| {
-            format!("UPDATE Alert of user {} \n{:#?}", user_id, alert_req)
-        });
-
     let delete_alert = warp::path!(String)
         .and(warp::delete())
         .and(dynamo_db)
-        .map(|user_id, _dynamo_db| format!("DELETE Alert of user {}", user_id));
+        .and_then(
+            |_user_id, dynamo_db: RetryingDynamoDb<DynamoDbClient>| async move {
+                let key = attr_map! {
+                    "user_id" => "abcdefgh".to_string()
+                };
+
+                dynamo_db
+                    .delete_item(DeleteItemInput {
+                        table_name: TABLE_NAME.to_string(),
+                        key,
+                        ..DeleteItemInput::default()
+                    })
+                    .await
+                    .map_err(build_err)?;
+                Ok::<_, warp::Rejection>(warp::reply::with_status(
+                    warp::reply::reply(),
+                    warp::http::StatusCode::NO_CONTENT,
+                ))
+            },
+        );
 
     warp::path!("alerts" / "register" / ..)
-        .and(get_alert.or(create_alert).or(update_alert).or(delete_alert))
+        .and(get_alert.or(create_alert).or(delete_alert))
         .with(warp::trace::named("alerts"))
+}
+
+fn build_err<E: Into<AlertError>>(err: E) -> warp::Rejection {
+    problem::build(err.into())
+}
+
+#[derive(Debug, Error)]
+pub enum AlertError {
+    #[error("unable to create alert")]
+    UnableToCreate(#[from] RusotoError<PutItemError>),
+    #[error("unable to get alert")]
+    UnableToGet(#[from] RusotoError<GetItemError>),
+    #[error("unable to parse attributes")]
+    UnableToParseAttr(#[from] AttributeError),
+    #[error("unable to find alert")]
+    NothingFound,
+    #[error("unable to get alert")]
+    UnableToDelete(#[from] RusotoError<DeleteItemError>),
 }
 
 #[derive(Debug, Deserialize)]
