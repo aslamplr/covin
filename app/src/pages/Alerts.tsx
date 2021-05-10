bf7a00c3999506a5c288ab97da9e54b7c03f1356
Aslam Ahammed <aslamplr@gmail.com>
2021-05-11 00:50:17 +0530

    Add centers api

--- app/src/pages/Alerts.tsx
+++ app/src/pages/Alerts.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { getAlert, Alert } from "../services/api";
+import { getAlert, Alert, CenterDim, getCenters } from "../services/api";
 import AlertEdit from "../components/alerts/AlertEdit";
 import AlertView from "../components/alerts/AlertView";
 
 export default function Alerts() {
   const [alert, setAlert] = React.useState<Alert | undefined>();
+  const [ centers, setCenters ] = React.useState<CenterDim[] | undefined>();
   const [isEdit, setEditing] = React.useState<boolean>(false);
   React.useEffect(() => {
     getAlert().then((alert) => {
       setAlert(alert);
     });
+    getCenters(296).then((centers) => {
+      setCenters(centers);
+    });
   }, []);
   return (
     <div>
+      <div>
+        <div className="m-7 bg-white shadow overflow-hidden sm:rounded-lg">
+          <pre className="m-2 p-2 text-blue-500">
+            {JSON.stringify(centers, null, 2)}
+          </pre>
+        </div>
+      </div>
       <div>
         <div className="m-7 bg-white shadow overflow-hidden sm:rounded-lg">
           <pre className="m-2 p-2 text-blue-500">
