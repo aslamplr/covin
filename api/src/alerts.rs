7ef87b266eb6ef679f727e2be93310a0a4f9fe94
Aslam Ahammed <aslamplr@gmail.com>
2021-05-16 03:45:15 +0530

    Minor refactoring: seperate out auth warp filter

--- api/src/alerts.rs
+++ api/src/alerts.rs
@@ -1,5 +1,5 @@
 use crate::{
-    auth::{self, AuthClaims},
+    auth::{warp_filter::auth_claims, AuthClaims},
     problem,
     validation::with_validated_json,
 };
@@ -17,62 +17,47 @@ use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use validator::Validate;
 use warp::Filter;
-use warp_lambda::lambda_http::request::RequestContext;
 
 const TABLE_NAME: &str = "CovinAlerts";
 
 pub fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let lambda_auth = warp::any()
-        .and(warp::filters::ext::get::<RequestContext>())
-        .and_then(|aws_req_context| async move {
-            tracing::debug!(message = "lambda request context");
-            auth::decode_auth_ctx(aws_req_context).map_err(problem::build)
-        });
-
-    let auth = warp::any()
-        .and(warp::header::<String>("authorization"))
-        .and_then(|token: String| async move {
-            tracing::debug!(message = "jwt token authentication");
-            auth::decode_token(&token).await.map_err(problem::build)
-        });
-
-    let auth = lambda_auth.or(auth).unify().map(|auth_claims| {
-        tracing::debug!(message = "auth claims intercept", claims = ?auth_claims);
-        auth_claims
-    });
+    let auth = auth_claims();
 
     let retry_policy = Policy::Pause(3, std::time::Duration::from_millis(10));
     let client = DynamoDbClient::new(Default::default()).with_retries(retry_policy);
     let dynamo_db = warp::any().map(move || client.clone());
 
-    let get_alert = warp::get().and(auth).and(dynamo_db.clone()).and_then(
-        |AuthClaims { user_id, .. }, dynamo_db: RetryingDynamoDb<DynamoDbClient>| async move {
-            let key = attr_map! {
-                "user_id" => user_id
-            };
+    let get_alert = warp::get()
+        .and(auth.clone())
+        .and(dynamo_db.clone())
+        .and_then(
+            |AuthClaims { user_id, .. }, dynamo_db: RetryingDynamoDb<DynamoDbClient>| async move {
+                let key = attr_map! {
+                    "user_id" => user_id
+                };
 
-            let res: AlertPayload = dynamo_db
-                .get_item(GetItemInput {
-                    table_name: TABLE_NAME.to_string(),
-                    key,
-                    ..GetItemInput::default()
-                })
-                .await
-                .map_err(build_err)
-                .map(|res| {
-                    res.item
-                        .map(|mut item| Alert::from_attrs(&mut item).map_err(build_err))
-                        .ok_or(AlertError::NothingFound)
-                        .map_err(build_err)
-                })???
-                .into();
-            Ok::<_, warp::Rejection>(warp::reply::json(&res))
-        },
-    );
+                let res: AlertPayload = dynamo_db
+                    .get_item(GetItemInput {
+                        table_name: TABLE_NAME.to_string(),
+                        key,
+                        ..GetItemInput::default()
+                    })
+                    .await
+                    .map_err(build_err)
+                    .map(|res| {
+                        res.item
+                            .map(|mut item| Alert::from_attrs(&mut item).map_err(build_err))
+                            .ok_or(AlertError::NothingFound)
+                            .map_err(build_err)
+                    })???
+                    .into();
+                Ok::<_, warp::Rejection>(warp::reply::json(&res))
+            },
+        );
 
     let create_alert = warp::post()
         .and(warp::path::end())
-        .and(auth)
+        .and(auth.clone())
         .and(with_validated_json())
         .and(dynamo_db.clone())
         .and_then(
