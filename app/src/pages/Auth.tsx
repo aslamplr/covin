e102245f553c7bb7373bd817f338bfd743007d0f
Aslam Ahammed <aslamplr@gmail.com>
2021-05-26 02:06:22 +0530

    Limit india country code for phone number
    - A naive and hacky implementation for now, this may not work for all!

--- app/src/pages/Auth.tsx
+++ app/src/pages/Auth.tsx
@@ -7,17 +7,42 @@ import {
 import { AuthState } from "@aws-amplify/ui-components";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 
-export default withRouter(({history, location}: RouteComponentProps) => {
+export default withRouter(({ history, location }: RouteComponentProps) => {
+  React.useEffect(() => {
+    // This is a hacky way to remove the rest of the country code from the the
+    // Amplify sign up component! not robust!!
+    // There are no option to limit country code in the component interface
+    // Or else we need to create a custom sign-up component!
+    // So, hack for the time being!! 😅
+    setTimeout(() => {
+      const countryCodeSelect = document
+        .querySelector("amplify-sign-up")
+        ?.shadowRoot?.querySelector("amplify-auth-fields")
+        ?.shadowRoot?.querySelector("amplify-phone-field")
+        ?.shadowRoot?.querySelector("amplify-form-field")
+        ?.querySelector("amplify-country-dial-code")
+        ?.shadowRoot?.querySelector("amplify-select")
+        ?.shadowRoot?.querySelector("select");
+      for (let i = countryCodeSelect?.children.length!; i >= 0; i--) {
+        const option = countryCodeSelect?.children[i];
+        if (option?.textContent !== "+91") {
+          option?.remove();
+        }
+      }
+    }, 2000);
+  }, []);
+
   const handleAuthStateChange = (nextAuthState: AuthState) => {
     if (nextAuthState === AuthState.SignedIn) {
       let redirectPath = "/";
-      const locationState = location.state as { from: { pathname: string }};
+      const locationState = location.state as { from: { pathname: string } };
       if (locationState && locationState.from) {
         redirectPath = locationState.from.pathname;
       }
       history.push(redirectPath);
     }
   };
+
   return (
     <AmplifyAuthenticator
       usernameAlias="email"
