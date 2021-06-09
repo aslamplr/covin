8fe054daa631620d540bd5c45bdc4ccd92150dd5
Aslam Ahammed <aslamplr@gmail.com>
2021-06-09 20:56:18 +0530

    Remove items to config

--- app/src/services/auth.ts
+++ app/src/services/auth.ts
@@ -1,11 +1,15 @@
 import Amplify, { Auth } from "aws-amplify";
 
+const COGNITO_REGION = process.env.REACT_APP_COGNITO_REGION;
+const COGNITO_POOL_ID = process.env.REACT_APP_COGNITO_POOL_ID;
+const COGNITO_WEB_CLIENT_ID = process.env.REACT_APP_COGNITO_WEB_CLIENT_ID;
+
 export const configure = () => {
   Amplify.configure({
     Auth: {
-      region: "ap-south-1",
-      userPoolId: "ap-south-1_0DvxhDRsV",
-      userPoolWebClientId: "68uau6menju7q3prl3t3gr1ksu",
+      region: COGNITO_REGION,
+      userPoolId: COGNITO_POOL_ID,
+      userPoolWebClientId: COGNITO_WEB_CLIENT_ID,
     },
   });
 };
